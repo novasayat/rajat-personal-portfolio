8aff2519221ba28ee2fd07d13a0640963abc73c8
Rajat Rautan <48512230+novasayat@users.noreply.github.com>
2026-02-02 15:17:58 -0500

    chore: initialize project structure

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import GlobalStyles from "./styles/GlobalStyles";
+import Cursor from "./components/Cursor/Cursor";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Cursor />
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
